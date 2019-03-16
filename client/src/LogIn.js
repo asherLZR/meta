797c1aaf1919db2393cfcce0619132dbff696292
Sally Yik <sallyyik@hotmail.com>
2019-03-16 21:52:50 +1100

    login page styling

--- client/src/LogIn.js
+++ client/src/LogIn.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { Row, Input, Icon, Button } from 'react-materialize'
+import { Row, Input, Icon, Button, Card, Col } from 'react-materialize'
+import './Login.css';
 
 export default class LogIn extends React.Component {
     constructor(props) {
@@ -33,20 +34,61 @@ export default class LogIn extends React.Component {
     render() {
         return(
             <div>
-                <h1>Log In</h1>
+                
                 <form className='logInForm'
                     onSubmit={ e=> {
                         e.preventDefault();
                         this.handleSubmit();
                     }}>
+
                     <Row>
-                        <Input placeholder='Username' s={12} label='Username' onChange={this.handleUsernameChange}><Icon medium>person</Icon></Input>
-                        <Input type='password' label="Password" s={12} onChange={this.handlePasswordChange}><Icon medium>lock</Icon></Input>
-                        <Button waves='light'>Submit</Button>
+                        <div className="vert-wrapper">
+                            <div className="pad"></div>
+                            <div>
+                                <Col s={4}></Col>
+                                <Col s={4}><h5>LOG IN</h5></Col>
+                                <Col s={4}></Col>
+                            </div>
+                            <div>
+                                <Col s={4}></Col>
+                                <Input placeholder='Username' s={4} label='Username' onChange={this.handleUsernameChange}><Icon medium>person</Icon></Input>
+                                <Col s={4}></Col>
+                            </div>
+                            <div>
+                                <Col s={4}></Col>
+                                <Input type='password' label="Password" s={4} onChange={this.handlePasswordChange}><Icon medium>lock</Icon></Input>
+                                <Col s={4}></Col>
+                            </div>
+                            <div className="pad"></div>
+                            <center><div><Button waves='light'>Submit</Button></div></center>
+                        </div>
                     </Row>
                 </form>
 
-            </div>
+                <div className="box"></div>
+            
+                    <div className="container">
+                        <div className="vert-wrapper">
+                        <center><img src="https://semantic-ui.com/images/avatar2/large/matthew.png" alt="W3Schools.com"></img>
+                            <h5>SAVE ARTICLES</h5>
+                            <div class="b">hello my name is bob and this is some cool text</div>
+                            </center>
+                        </div>
+                        <div className="vert-wrapper">
+                        <center><img src="https://semantic-ui.com/images/avatar2/large/matthew.png" alt="W3Schools.com"></img>
+                            <h5>ANALYSE</h5>
+                            <div class="b">hello my name is bob and this is some cool text</div>
+                            </center>
+                        </div>
+                        <div className="vert-wrapper">
+                            <center><img src="https://semantic-ui.com/images/avatar2/large/matthew.png" alt="W3Schools.com"></img>
+                            <h5>SET GOALS</h5>
+                            <div class="b">hello my name is bob and this is some cool text</div>
+                            </center>
+                        </div>
+                    </div>
+
+            </div>            
         )
     }
 }
