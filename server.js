83bfc71fcd6047646f48c3fdeac518be307d4c6a
asherLZR <asher.lzrz@gmail.com>
2019-03-17 03:28:21 +1100

    Fixed goals api

--- server.js
+++ server.js
@@ -120,6 +120,7 @@ app.get('/api/v1/stats/goals', function (req, res) {
     p.stdout.on('data', (data)=>res.send(data));
 });
 
+
 app.post('/api/v1/account/signup', function(req, res) {
     let username = req.body.username;
     let password = req.body.password;
