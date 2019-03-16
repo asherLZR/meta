088319f9882ebc96c85b66c7739ca34556d00b23
khanguslee <angus.lee809@gmail.com>
2019-03-16 17:54:12 +1100

    Send login details to server

--- client/src/LogIn.js
+++ client/src/LogIn.js
@@ -1,21 +1,50 @@
 import React from 'react';
 
-import { Row, Input, Icon } from 'react-materialize'
+import { Row, Input, Icon, Button } from 'react-materialize'
 
-/*
-    For now, the about me page will be the home page
-*/
 export default class LogIn extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+        };
+    };
+    handleUsernameChange = (e) => {
+        this.setState({ username: e.currentTarget.value });
+    }
+    handlePasswordChange = (e) => {
+        this.setState({ password: e.currentTarget.value });
+    }
+
+    handleSubmit = () => {
+        // Send login details to server
+        let requestOptions = {
+            method: 'POST',
+            body: JSON.stringify(this.state),
+            headers: {'Content-Type': 'application/json'},
+        };
+        fetch('/api/v1/login', requestOptions)
+            .then(res => res.json())
+            .then(response => { console.log(JSON.stringify(response))})
+            .catch(error => console.error(`Error: ${error}`));
+    }
+
     render() {
         return(
             <div>
                 <h1>Log In</h1>
-                <div className='logInForm'>
+                <form className='logInForm'
+                    onSubmit={ e=> {
+                        e.preventDefault();
+                        this.handleSubmit();
+                    }}>
                     <Row>
-                        <Input placeholder='Username' s={12} label='Username'><Icon medium>person</Icon></Input>
-                        <Input type='password' label="Password" s={12}><Icon medium>lock</Icon></Input>
+                        <Input placeholder='Username' s={12} label='Username' onChange={this.handleUsernameChange}><Icon medium>person</Icon></Input>
+                        <Input type='password' label="Password" s={12} onChange={this.handlePasswordChange}><Icon medium>lock</Icon></Input>
+                        <Button waves='light'>Submit</Button>
                     </Row>
-                </div>
+                </form>
 
             </div>
         )
