1470937f6de7a4b5c217d30a9b4a807a73480777
asherLZR <asher.lzrz@gmail.com>
2019-03-17 10:14:56 +1100

    Add collections to home

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,21 +1,55 @@
 import React from 'react';
-import { Row, Col } from 'react-materialize'
+import { Collection, CollectionItem ,Chip} from 'react-materialize'
 
 import LogOut from './LogOut.js';
 
-export default class Home extends React.Component {
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
+
+
+class ArticleItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { article: props.article };
+    }
+
     render() {
-        return(
-            <div>
-                <Row>
-                    <Col s={4}/>
-                    <Col s={4}><h5>Home</h5></Col>
-                    <Col s={4}/>
-                </Row>
-                <Row>
-                    <LogOut />
-                </Row>
+        return <CollectionItem>
+        <div>
+            <h5>{this.state.article.category[0].replaceAll('\"','')}</h5>
+            <div>{".. " + this.state.article.article_snippet + " .."}</div>
+            <div><Chip><img src='//logo.clearbit.com/abc.net.au' alt='News Icon'/>abcnews</Chip></div>
             </div>
-        )
+        </CollectionItem>;
+    }
+}
+
+export default class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { articleList: [] };
+    }
+
+    componentDidMount() {
+        fetch('/api/v1/stats/collection?username=' + 'hdas')
+            .then(res => res.json())
+            .then(response => {
+                console.log(response);
+                this.setState({articleList: response});
+            })
+            .catch(error => console.error(`Error: ${error}`));
+    }
+
+    render() {
+        return (
+            <div>
+                <Collection>{this.state.articleList.map(function(x) {
+                    console.log(x.source);
+                    return <ArticleItem article={x}/>
+                })}</Collection>
+            </div>
+        );
     }
 }
