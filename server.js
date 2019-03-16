1cfff72eb7406836b373ae58c231b9d526829056
harsilspatel <harsilspatel@gmail.com>
2019-03-16 16:04:06 +1100

    Add stats api and dummy data

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -8,15 +10,34 @@ const app = express();
 // Host static files (Front-end files)
 app.use(express.static(path.join(__dirname,'client','build')));
 
+// For parsing application/x-www-form-urlencoded
+app.use(bodyParser.json()); // for parsing application/json
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 /* ENDPOINTS */
 app.get('/', (req, res) => {
     let frontPagePath = path.join(__dirname, 'client', 'build', 'index.html');
     res.send(frontPagePath);
 });
 
-app.get('/', (req, res) => {
-    let frontPagePath = path.join(__dirname, 'client', 'build', 'index.html');
-    res.send(frontPagePath);
-});
+app.post('/api/v1/upload', function (req, res) {
+    // TODO: Insert into MongoDB database
+    console.log(req.body.user);
+    console.log(req.body.url);
+    // TODO: call python scripts
+    res.end()
+    // console.log(req.user);
+    // console.log(req.url);
+  });
+
+app.get('/api/v1/stats/progress', function (req, res) {
+    res.sendFile(path.join(__dirname, 'dummy_data', 'progress.json'))
+})
+
+app.get('/api/v1/stats/goals', function (req, res) {
+    res.sendFile(path.join(__dirname, 'dummy_data', 'goals.json'))
+})
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
